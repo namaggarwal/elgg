f46dc041a9478063c8caaca60ff970b656c7e79f
Naman Aggarwal <nam.aggarwal@yahoo.com>
2014-02-22 04:09:46 +0800

    newletter changes

--- mod/newsletters/start.php
+++ mod/newsletters/start.php
@@ -27,9 +27,7 @@
 	 **/
 	function newsletters_page_handler($page)
 	{	
-		$period=minute;
-		register_plugin_hook('cron', $period, 'logrotate_cron');
-
+		
 		//TODO  : Set actions for other pages such as add and modify newsletters here.
 
 		switch ($page[0]) {
@@ -51,8 +49,13 @@
 				break;
 
 
-			case 'run':
-				mail("naman.coool@gmail.com","Naman","naman123");
+			case 'sendNewsLetter':
+				if(isset($page[1])){
+					set_input('id',$page[1]);
+					set_input('action',$page[0]);
+				}else{
+					return false;
+				}
 				break;
 
 
@@ -65,10 +68,6 @@
 		return true;	
 	}
 
-	function logrotate_cron($hook, $entity_type, $returnvalue, $params){
-		print('Executing cron');
-		$resulttext = elgg_echo("Executing!!");
-	}
 
 	register_elgg_event_handler('init', 'system', 'newsletters_init');
 
