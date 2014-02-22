a007f03f93c88c66d2e6a75f911eebfc84cef6dd
Naman Aggarwal <nam.aggarwal@yahoo.com>
2014-02-22 20:53:59 +0800

    File tagging notification added

--- mod/tagging/actions/tag.php
+++ mod/tagging/actions/tag.php
@@ -12,5 +12,49 @@ $file=get_entity($fid);
 
 $file->friendtags=implode(",",$arr);	
 $file->save();
+
+//Send notitfication
+$url = 'http://localhost:1337/notifyforme';
+$fid = $file->friendtags;
+$myid = $file->owner_id;
+$message = "You have been tagged in a file";
+$link  = "www.google.com";
+if($fid!=""){
+
+	$fields = array(
+						'myid' => urlencode($myid),
+						'fid' => urlencode($fid),						
+						'message' => urlencode($message),
+						'link' => urlencode($link),
+
+				);
+	$and = "";
+	//url-ify the data for the POST
+	foreach($fields as $key=>$value) { 
+		$fields_string .= $and.$key.'='.$value; 
+		$and = "&";
+	}
+	rtrim($fields_string,'&');
+
+	//open connection
+	$ch = curl_init();
+
+	//set the url, number of POST vars, POST data
+	curl_setopt($ch,CURLOPT_URL, $url);
+	curl_setopt($ch,CURLOPT_POST, count($fields));
+	curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+	//execute post
+	$result = curl_exec($ch);
+
+	//close connection
+	curl_close($ch);
+
+
+}
+
+
+
+
 forward($_SERVER['HTTP_REFERER']);
 ?>
