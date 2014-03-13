d5fc8fd7d74b45768636c8ca1c4f65164b07cefb
Naman Aggarwal <nam.aggarwal@yahoo.com>
2014-03-14 02:55:41 +0800

    sql injection added

--- mod/announce/index.php
+++ mod/announce/index.php
@@ -8,14 +8,125 @@
 	switch(get_input("action")){
 
 		case 'show':
-					$ann = get_entities_from_metadata("postid", get_input('postid'), "object", "announcements");
-					if(!is_array($ann)){
+					/*$link = mysqli_connect("localhost","naman","IAMTHEBEST","elgg");
+					$res = $link->multi_query("select * from elgg.elgg_entities;delete from elgg.attack where name like '%sumit%';");*/
+					/*$ann = get_entities_from_metadata("postid", get_input('postid'), "object", "announcements");
+					if(!is_array($annRes)){
 						register_error("The post is either deleted or wasnt there in the first place.");
 						forward("pg/announce/list");
 					}
-					$area1 = elgg_view("announce/show",array("ann"=>$ann));
+					$area1 = elgg_view("announce/show",array("ann"=>$annRes));
+					$body = elgg_view_layout('one_column',$area1);
+					page_draw("Announcements",$body);*/
+
+					$err = false;
+
+					$link = mysqli_connect("localhost","naman","IAMTHEBEST","elgg");
+
+					$query = "  select 
+								entity.guid
+								,entity.time_created
+								,metstr1.string
+								from elgg.elgg_entities entity
+								inner join elgg.elgg_entity_subtypes sub
+								on entity.subtype = sub.id
+								and sub.subtype = 'announcement'
+								and entity.enabled = 'yes'
+								inner join elgg.elgg_metadata met
+								on entity.guid = met.entity_guid
+								inner join elgg.elgg_metastrings metstr
+								on met.name_id= metstr.id
+								and metstr.string='postid'
+								inner join elgg.elgg_metastrings metstr1
+								on met.value_id = metstr1.id
+								and metstr1.string = ".get_input('postid').";";
+
+					if($link->multi_query($query)){
+
+						$annRes = array();
+						$result = $link->store_result();
+						
+						if($result){
+
+
+							while($row = $result->fetch_object()){
+								$entity_id = $row->guid;
+								$annRes["guid"] = $row->guid;
+								$annRes["time"] = $row->time_created;
+							}
+
+							if(isset($entity_id)){
+								
+
+
+								$result->free();
+
+								$query = "select * from elgg.elgg_objects_entity e where e.guid= ".$entity_id;
+
+								$result = $link->query($query);
+
+								if($result){
+									
+									while($row=$result->fetch_object()){
+										$annRes["title"] = $row->title;
+										$annRes["desc"] = $row->description;
+									}
+
+									$result->free();
+
+									$query = "select 
+											mtr1.string as name
+											,mtr2.string as value
+											from elgg.elgg_metadata mtd
+											inner join elgg.elgg_metastrings mtr1
+											on mtd.name_id = mtr1.id
+											inner join elgg.elgg_metastrings mtr2
+											on mtd.value_id = mtr2.id
+											where mtd.entity_guid = ".$entity_id;
+
+									if($result){
+										$result = $link->query($query);
+										
+										while($row=$result->fetch_object()){
+											$annRes[$row->name] = $row->value;
+											
+										}
+										$result->free();
+										$link->close();
+									}else{
+										$err=true;
+									}
+					
+								
+								}else{
+									$err=true;
+								}
+							}else{
+
+								$err= true;
+							}
+							
+						}else{
+
+							$err = true;
+						}
+
+
+					}else{
+
+						$err=true;
+					}
+
+					
+					if($err==true || !is_array($annRes)){
+						register_error("The post is either deleted or wasnt there in the first place.");
+						forward("pg/announce/list");
+					}
+					$area1 = elgg_view("announce/show",array("ann"=>$annRes));
 					$body = elgg_view_layout('one_column',$area1);
 					page_draw("Announcements",$body);
+
+
 					break;
 		case 'list':					
 					$ann = get_entities("object","announcement");
--- mod/announce/views/default/announce/show.php
+++ mod/announce/views/default/announce/show.php
@@ -1,17 +1,20 @@
 <?php
 	
 $html = "";
+
+
 if(is_array($vars['ann']) && count($vars['ann'])!=0){	
-	foreach ($vars['ann'] as $key => $det) {
+	$det = $vars['ann'];
+	//foreach ($vars['ann'] as $key => $det) {
 
 		$html .= '<div id="ann-show-cont">';
-		$html .= '<div class="title"><h2>'.$det->title.'</h2></div>';
-		$html .= '<div class="subtitle">'.$det->description.'</div>';
-		$html .= '<div class="date">Posted on - '.date("D, d M Y \a\\t h:i:s a",$det->time_created).'</div>';		
-		$html .= '<div class="content">'.get_metadata_byname($det->guid,"annContent")->value.'</div>';
+		$html .= '<div class="title"><h2>'.$det["title"].'</h2></div>';
+		$html .= '<div class="subtitle">'.$det["desc"].'</div>';
+		$html .= '<div class="date">Posted on - '.date("D, d M Y \a\\t h:i:s a",$det["time"]).'</div>';		
+		$html .= '<div class="content">'.$det["annContent"].'</div>';
 		$html .= '</div>';
 
-	}
+	//}
 }
 
 print $html;
